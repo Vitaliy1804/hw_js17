9a06f0c9f86433c9796d8b91cdbd4ad3027af04c
Vitaliy Scheremet <shervit82@gmail.com>
2019-06-01 09:18:57 +0300

    fst

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#draw').click(function(){
+        $('#draw').remove()
+       let diameterCircle =   $('<input type="text" class="diametr-circle" placeholder="Введите диаметр круга в px">')
+        let colorCircle =   $('<input type="text" class="color-circle" placeholder="Введите цвет круга">')
+        let draw = $('<button id="draw">Нарисовать</button>');
+        $('body').append(diameterCircle,colorCircle,draw);
+        let pixelValue = +(diameterCircle.val());
+        let colorValue = colorCircle.val();
+        
+        draw.click(function(){
+            let circle = $('<div> </div>').css({backgroundColor:colorCircle.val(), 
+                height: diameterCircle.val(), width: diameterCircle.val(), 
+                margin:"20px auto", "border-radius": "50%"})
+            $('body').append(circle)
+        })
+    })
+})
+
